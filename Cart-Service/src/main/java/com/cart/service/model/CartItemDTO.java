e4494565e637b67749e5769dd68ee1ca44195089
Akshay-Luhaidya-1999 <150114444+Akshay-Luhaidya-1999@users.noreply.github.com>
2023-12-12 14:11:32 +0530

    Created Cart Service

---
+++ Cart-Service/src/main/java/com/cart/service/model/CartItemDTO.java
@@ -0,0 +1,49 @@
+package com.cart.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class CartItemDTO {
+	
+	private Long bookId;
+    private String title;
+    private int quantity;
+    private double price;
+	public Long getBookId() {
+		return bookId;
+	}
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+    
+
+}
---
+++ Cart-Service/src/main/java/com/cart/service/serviceImpl/CartService.java
@@ -0,0 +1,47 @@
+package com.cart.service.serviceImpl;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cart.service.model.Cart;
+import com.cart.service.model.CartItem;
+import com.cart.service.model.CartItemDTO;
+import com.cart.service.repository.CartRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+	
+	@Autowired
+    private CartRepository cartRepository;
+
+	public Cart addToCart(Long userId, CartItemDTO cartItemDTO) {
+        Cart cart = cartRepository.findByUserId(userId)
+                                  .orElseGet(() -> new Cart(userId));
+
+        CartItem cartItem = mapCartItemDTOToEntity(cartItemDTO);
+        cart.addItem(cartItem);
+
+        return cartRepository.save(cart);
+    }
+
+    public Cart getCartByUserId(Long userId) {
+        return cartRepository.findByUserId(userId)
+                             .orElseThrow(() -> new NoSuchElementException("Cart not found for user ID: " + userId));
+    }
+
+    // Helper method to map CartItemDTO to CartItem entity
+    private CartItem mapCartItemDTOToEntity(CartItemDTO cartItemDTO) {
+        CartItem cartItem = new CartItem();
+        cartItem.setBookId(cartItemDTO.getBookId());
+        cartItem.setTitle(cartItemDTO.getTitle());
+        cartItem.setQuantity(cartItemDTO.getQuantity());
+        cartItem.setPrice(cartItemDTO.getPrice());
+        return cartItem;
+    }
+}
+
